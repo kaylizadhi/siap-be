407264fdf6ab2fb1fe3f42904b33e823f92481b3
rezataufiq56 <rezataufiq56@gmail.com>
2024-10-07 13:37:56 +0700

    Mengerjakan konfigurasi postgresql

--- klien/views.py
+++ klien/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import DataKlien
 
-# Create your views here.
+# Membuat instance sementara tanpa menyimpan ke database
+data_klien = DataKlien(
+    nama_klien="John Doe",
+    nama_perusahaan="Tech Corp",
+    daerah="Jakarta",
+    harga_survei=100000
+)
+
+# Menampilkan data
+print(data_klien.nama_klien)
+print(data_klien.nama_perusahaan)
+print(data_klien.daerah)
+print(data_klien.harga_survei)
