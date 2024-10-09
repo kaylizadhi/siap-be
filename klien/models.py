54c0bf174c967a0e5cc0fc6a135b578bea61b3c2
Reza Taufiq Yahya <reza.taufiq@ui.ac.id>
2024-10-09 10:08:30 +0700

    Sprint1 reza

--- klien/models.py
+++ klien/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class DataKlien(models.Model):
     nama_klien = models.CharField(max_length=50)
-    nama_perusahaan = models.CharField(max_length=50)
+    nama_perusahaan = models.CharField(max_length=100)
     daerah = models.TextField()
     harga_survei = models.IntegerField()
