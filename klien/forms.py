0a03ef9cf26e00edfc2c8c761008fd29231d2e1d
unknown <alyssa.larasati@ui.ac.id>
2024-10-30 17:04:17 +0700

    push buatAkun dan DaftarAkun

--- klien/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import DataKlien
-
-class DataKlienForm(forms.ModelForm):
-    class Meta:
-        model = DataKlien
-        fields = ['nama_klien', 'nama_perusahaan', 'daerah', 'harga_survei']
--- klien/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class DataKlien(models.Model):
-    nama_klien = models.CharField(max_length=50)
-    nama_perusahaan = models.CharField(max_length=100)
-    daerah = models.TextField()
-    harga_survei = models.IntegerField()
