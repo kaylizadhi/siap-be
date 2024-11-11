dcf80336f328a6681087a8e6c780b76182e9fac5
Mathilda Dellanova <mathilda.dellanova@ui.ac.id>
2024-11-11 11:35:12 +0700

    fixing minor problem

--- souvenir/urls.py
+++ souvenir/urls.py
@@ -4,7 +4,6 @@ from .views import *
 
 urlpatterns=[
     path("get-list-souvenir",get_list_souvenir),
-    path("get-souvenir-detail/<str:id>/",get_souvenir_detail),
     path("add-souvenir/",add_souvenir),
     path("update-souvenir/<str:id>/",update_souvenir),
     path("delete-souvenir/<str:id>/",delete_souvenir),
