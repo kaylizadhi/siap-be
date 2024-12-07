a052ab3fd6adcc54ab7f17086be4bed10b191893
Mathilda Dellanova <mathilda.dellanova@ui.ac.id>
2024-12-08 02:45:04 +0700

    fix migration survei & klien urls

--- project_django/urls.py
+++ project_django/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('api/survei/',include('survei.urls')),
-    path('klien/', include('klien.urls')),
+    path('api/klien/', include('klien.urls')),
     path('api/survei-status/', include('tracker_survei.urls')),
     path('dokumen_pendukung/', include('dokumen_pendukung.urls')),
     path('api/souvenir/', include('souvenir.urls')),
--- survei/views.py
+++ survei/views.py
@@ -65,27 +65,6 @@ def delete_survei(request, id):
     survei.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
 
-# @api_view(['GET'])
-# def get_location_data(request):
-#     param = request.query_params.get('param', '').lower()
-
-#     if param == 'provinsi':
-#         data = [
-#             {"id": "Jawa Barat", "name": "Jawa Barat"},
-#             {"id": "Jawa Tengah", "name": "Jawa Tengah"},
-#             {"id": "Jawa Timur", "name": "Jawa Timur"},
-#         ]
-#     elif param == 'kabupaten/kota':
-#         data = [
-#             {"id": "Bandung", "name": "Bandung"},
-#             {"id": "Semarang", "name": "Semarang"},
-#             {"id": "Surabaya", "name": "Surabaya"},
-#         ]
-#     else:
-#         return Response({"error": "Parameter tidak valid. Gunakan 'provinsi' atau 'kabupaten/kota'."}, status=400)
-
-#     return Response(data)
-
 @api_view(['GET'])
 def get_survei_count_by_region(request):
     ruang_lingkup = request.query_params.get('ruang_lingkup', None)
@@ -124,46 +103,35 @@ def get_survei_count_by_region(request):
         "Sumatera Barat": "ID.SB",
         "Sumatera Selatan": "ID.SS",
         "Sumatera Utara": "ID.SU",
-        "Yogyakarta": "ID.YO"
+        "Yogyakarta": "ID.YO",
     }
 
-    # counts = (
-    #     Survei.objects.values("wilayah_survei")
-    #     .annotate(survey_count=Count("id"))
-    #     .order_by("wilayah_survei")
-    # )
+    # Daftar kota manual
+    CITIES = [
+        {"id": "ID.JK.01", "name": "Jakarta Pusat", "province": "Jakarta"},
+        {"id": "ID.JK.02", "name": "Jakarta Utara", "province": "Jakarta"},
+        {"id": "ID.JK.03", "name": "Jakarta Barat", "province": "Jakarta"},
+        {"id": "ID.JK.04", "name": "Jakarta Selatan", "province": "Jakarta"},
+        {"id": "ID.JK.05", "name": "Jakarta Timur", "province": "Jakarta"},
+        {"id": "ID.JB.01", "name": "Bandung", "province": "Jawa Barat"},
+        {"id": "ID.JB.02", "name": "Bogor", "province": "Jawa Barat"},
+        {"id": "ID.JB.03", "name": "Bekasi", "province": "Jawa Barat"},
+    ]
 
-    # data = []
-    # for region in counts:
-    #     region_code = REGION_CODE_MAPPING.get(region["wilayah_survei"])
-    #     if region_code:
-    #         data.append({"id": region_code, "value": region["survey_count"]})
-
-    # Fetch all surveys
-    surveys = Survei.objects.all()
-
-    # Initialize a dictionary to count surveys per region
-    region_counts = {}
-    
     if ruang_lingkup == "Nasional":
-        nasional_surveys = surveys.filter(ruang_lingkup="Nasional")
-        for region_code in REGION_CODE_MAPPING.values():
-            region_counts[region_code] = len(nasional_surveys)
+        data = [{"id": "ID", "value": "Indonesia"}]
+        return Response(data, status=status.HTTP_200_OK)
+
+    elif ruang_lingkup == "Provinsi":
+        data = [{"id": code, "value": name} for name, code in REGION_CODE_MAPPING.items()]
+        return Response(data, status=status.HTTP_200_OK)
+
+    elif ruang_lingkup == "Kota":
+        data = [{"id": city["id"], "value": city["name"]} for city in CITIES]
+        return Response(data, status=status.HTTP_200_OK)
+
     else:
-        if ruang_lingkup:
-            surveys = surveys.filter(ruang_lingkup=ruang_lingkup)
-    # Process each survey
-        for survey in surveys:
-            province_found = None
-            for region_name in survey.wilayah_survei:  # `wilayah_survei` is a list
-                if region_name in REGION_CODE_MAPPING:
-                    province_found = region_name
-                    break
-            if province_found:
-                region_code = REGION_CODE_MAPPING[province_found]
-                region_counts[region_code] = region_counts.get(region_code, 0) + 1
-
-    # Transform the result into a list for the response
-    data = [{"id": region, "value": count} for region, count in region_counts.items()]
-
-    return Response(data, status=status.HTTP_200_OK)
+        return Response(
+            {"error": "Invalid ruang_lingkup parameter"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
