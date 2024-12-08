66a7c28a513f3dbaf08ea56323321f6996730667
Hanin Atina Rahmania <hanin.atina@ui.ac.id>
2024-12-09 06:38:01 +0700

    added url export existing kwt final

--- dokumen_pendukung/urls.py
+++ dokumen_pendukung/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import generate_invoice_dp, generate_invoice_final, download_template_proposal, upload_template_proposal, download_template_kontrak, upload_template_kontrak, generate_kwitansi_dp, generate_kwitansi_final, export_existing_invoice_dp, export_existing_invoice_final, export_existing_kwitansi_dp
+from .views import generate_invoice_dp, generate_invoice_final, download_template_proposal, upload_template_proposal, download_template_kontrak, upload_template_kontrak, generate_kwitansi_dp, generate_kwitansi_final, export_existing_invoice_dp, export_existing_invoice_final, export_existing_kwitansi_dp, export_existing_kwitansi_final
 
 urlpatterns = [
     path('generate_invoice_dp/', generate_invoice_dp, name='generate_invoice_dp'),
@@ -13,4 +13,5 @@ urlpatterns = [
     path('export_existing_invoice_dp/', export_existing_invoice_dp, name='export_existing_invoice_dp'),
     path('export_existing_invoice_final/', export_existing_invoice_final, name='export_existing_invoice_final'),
     path('export_existing_kwitansi_dp/', export_existing_kwitansi_dp, name='export_existing_kwitansi_dp'),
+    path('export_existing_kwitansi_final/', export_existing_kwitansi_final, name='export_existing_kwitansi_final'),
 ]
